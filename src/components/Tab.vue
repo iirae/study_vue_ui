9d3fadeadb4a6effa076a54c1e87b125d7b876d1
Rony <74164615+rony-inbyu@users.noreply.github.com>
2021-11-04 18:59:16 +0900

    탭 데코 작업 중

--- src/components/Tab.vue
+++ src/components/Tab.vue
@@ -7,7 +7,7 @@
                 <a @click="onClickTab(tabmenu.id)" v-html="tabmenu.label"></a>
             </li>
         </ul>
-
+        <span class="tab-point"></span>
     </div>
 </template>
 
@@ -29,11 +29,37 @@
         },
         methods: {
             init() {
+                this.getSelectedTabWidth();
+                // const $this = $(e.target);
+                // const $tabItem = $this.closest('li'),
+                //     $wrap = $this.closest('.module-tab');
 
+                // let tabItemWidth = $this.width();
+                // let tabItemPositionLeft = $tabItem.position().left + $this.closest('.tab').scrollLeft();
+
+                // function tabDeco($wrap, tabItemWidth, tabItemOffsetLeft) {
+                    // $($wrap).find('.tab-point').css({
+                    //     'width': tabItemWidth,
+                    //     'transform': ('translate3d(' + (tabItemOffsetLeft) + 'px, 0, 0)')
+                    // });
+                // }
             },
             onClickTab(tabId) {
                 // console.log('onClickTab', tabId);
                 this.$emit('selectedTab', tabId);
+                this.getSelectedTabWidth();
+            },
+            getSelectedTabWidth() {
+                let tabInfo = document.querySelector('li.on').getBoundingClientRect();
+                let tabWidth = tabInfo.width;
+                let tabPositionLeft = tabInfo.left;
+
+                const tabPoint = document.querySelector('.tab-point');
+
+                tabPoint.style.cssText = 'width:' + tabWidth + 'px; transform: translate3d(' + tabPositionLeft + 'px, 0, 0)';
+                // tabPoint.style.width = tabWidth;
+                // tabPoint.style.transform = 'translate3d(' + tabPositionLeft + 'px, 0, 0';
+                console.log(tabWidth, tabPositionLeft);
             }
         }
     }
